c9d78cd94566dcf455a1847221ec8e2caf84ca06
VasylDiakulych <vasyadiakulych@gmail.com>
2026-07-05 11:16:53 +0200

    restructure

---
+++ include/terrain_generation.hpp
@@ -0,0 +1,13 @@
+#ifndef TERRAIN_GENERATION_HPP
+#define TERRAIN_GENERATION_HPP
+
+#include "generics.hpp"
+#include "../third_party/PerlinNoise.hpp"
+
+struct basicGenerator {
+    siv::PerlinNoise noise{42};
+
+    
+};
+
+#endif
