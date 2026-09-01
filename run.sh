06873e9268d83786c8cfe50b56695b92335918be
VasylDiakulych <vasyadiakulych@gmail.com>
2026-09-01 23:20:03 +0200

    added submodule init to run script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+git submodule update --init --recursive
+
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DSANITIZE=OFF --log-level=ERROR
 cmake --build build --target viz
 exec ./build/viz "$@"
